1dc4efa15940dc66af939998fc48cdba797a7078
Mozz123 <68307526+Mozz123@users.noreply.github.com>
2021-01-10 19:44:35 -0700

    Made MasterSwordBeamEntity.java cleaner. Still buggy.

--- src/main/java/botwmod/entity/projectile/MasterSwordBeamEntity.java
+++ src/main/java/botwmod/entity/projectile/MasterSwordBeamEntity.java
@@ -9,14 +9,17 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.entity.projectile.AbstractArrowEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.entity.projectile.ProjectileHelper;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.IPacket;
 import net.minecraft.network.play.server.SChangeGameStatePacket;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.Hand;
-import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.EntityRayTraceResult;
 import net.minecraft.util.math.MathHelper;
@@ -32,15 +35,18 @@ import net.minecraftforge.fml.network.NetworkHooks;
 import java.util.List;
 
 public class MasterSwordBeamEntity extends AbstractArrowEntity {
-    public MasterSwordBeamEntity(EntityType type, World worldIn) {
-        super(type, worldIn);
+
+    protected int lifespan = 300;
+
+    public MasterSwordBeamEntity (EntityType type, World world) {
+        super(type, world);
         this.setDamage(5F);
     }
 
     public MasterSwordBeamEntity(EntityType type, World worldIn, double x, double y, double z, float r, float g, float b) {
         this(type, worldIn);
         this.setPosition(x, y, z);
-        this.setDamage(9F);
+        this.setDamage(5F);
     }
 
     public MasterSwordBeamEntity(EntityType type, World worldIn, LivingEntity shooter, double dmg) {
@@ -52,111 +58,63 @@ public class MasterSwordBeamEntity extends AbstractArrowEntity {
         this(ModEntities.MASTER_SWORD.get(), worldIn);
     }
 
-    public boolean isInWater() {
-        return false;
+    @Override
+    public void tick() {
+        Entity thrower = func_234616_v_();
+        if (thrower instanceof net.minecraft.entity.player.PlayerEntity && !thrower.isAlive()) {
+            remove();
+            return;
+        }
+        // remove if too old
+        if(this.ticksExisted > lifespan) {
+            remove();
+            return;
+        }
+        // check for impact
+        if(!this.getEntityWorld().isRemote()) {
+            RayTraceResult raytraceresult = ProjectileHelper.func_234618_a_(this, this::func_230298_a_);
+            if (raytraceresult != null && raytraceresult.getType() != RayTraceResult.Type.MISS
+                    && !net.minecraftforge.event.ForgeEventFactory.onProjectileImpact(this, raytraceresult)) {
+                this.onImpact(raytraceresult);
+            }
+        }
+        // movement
+        Vector3d motion = this.getMotion();
+        double d0 = this.getPosX() + motion.x;
+        double d1 = this.getPosY() + motion.y;
+        double d2 = this.getPosZ() + motion.z;
+        // lerp rotation and pitch
+        this.func_234617_x_();
+        // actually move the entity
+        this.setPosition(d0, d1, d2);
+        // super method
+        super.tick();
     }
 
     @Override
-    protected void registerData() {
-        super.registerData();
+    protected void onEntityHit(EntityRayTraceResult p_213868_1_) {
+        super.onEntityHit(p_213868_1_);
+        Entity entity = p_213868_1_.getEntity();
+        entity.attackEntityFrom(DamageSource.causeIndirectMagicDamage(this, this.func_234616_v_()), (float) this.getDamage());
     }
 
-    public void tick() {
-        super.tick();
-        noClip = false;
-        float sqrt = MathHelper.sqrt(this.getMotion().x * this.getMotion().x + this.getMotion().z * this.getMotion().z);
-        if ((sqrt < 0.1F) && this.ticksExisted > 200) {
-            this.remove();
-        }
-        double d0 = 0;
-        double d1 = 0.0D;
-        double d2 = 0.01D;
-        double x = this.getPosX() + (double) (this.rand.nextFloat() * this.getWidth() * 2.0F) - (double) this.getWidth();
-        double y = this.getPosY() + (double) (this.rand.nextFloat() * this.getHeight()) - (double) this.getHeight();
-        double z = this.getPosZ() + (double) (this.rand.nextFloat() * this.getWidth() * 2.0F) - (double) this.getWidth();
-        float f = (this.getWidth() + this.getHeight() + this.getWidth()) * 0.333F + 0.5F;
-        if (particleDistSq(x, y, z) < f * f) {
-            this.world.addParticle(ParticleTypes.SNEEZE, x, y + 0.5D, z, d0, d1, d2);
-        }
-        Vector3d vector3d = this.getMotion();
-        float f3 = MathHelper.sqrt(horizontalMag(vector3d));
-        this.rotationYaw = (float)(MathHelper.atan2(vector3d.x, vector3d.z) * (double)(180F / (float)Math.PI));
-        this.rotationPitch = (float)(MathHelper.atan2(vector3d.y, (double)f3) * (double)(180F / (float)Math.PI));
-        this.prevRotationYaw = this.rotationYaw;
-        this.prevRotationPitch = this.rotationPitch;
-        Vector3d vector3d2 = this.getPositionVec();
-        Vector3d vector3d3 = vector3d2.add(vector3d);
-        RayTraceResult raytraceresult = this.world.rayTraceBlocks(new RayTraceContext(vector3d2, vector3d3, RayTraceContext.BlockMode.COLLIDER, RayTraceContext.FluidMode.NONE, this));
-        if (raytraceresult.getType() != RayTraceResult.Type.MISS) {
-            vector3d3 = raytraceresult.getHitVec();
-        }
-        while(!this.removed) {
-            EntityRayTraceResult entityraytraceresult = this.rayTraceEntities(vector3d2, vector3d3);
-            if (entityraytraceresult != null) {
-                raytraceresult = entityraytraceresult;
-            }
+    @Override
+    protected ItemStack getArrowStack() {
+        return ItemStack.EMPTY;
+    }
 
-            if (raytraceresult != null && raytraceresult.getType() == RayTraceResult.Type.ENTITY) {
-                Entity entity = ((EntityRayTraceResult)raytraceresult).getEntity();
-                Entity entity1 = this.func_234616_v_();
-                if (entity instanceof PlayerEntity && entity1 instanceof PlayerEntity && !((PlayerEntity)entity1).canAttackPlayer((PlayerEntity)entity)) {
-                    raytraceresult = null;
-                    entityraytraceresult = null;
-                }
-            }
-
-            if (raytraceresult != null && raytraceresult.getType() != RayTraceResult.Type.MISS && !net.minecraftforge.event.ForgeEventFactory.onProjectileImpact(this, raytraceresult)) {
-                if(raytraceresult.getType() != RayTraceResult.Type.BLOCK){
-                    this.onImpact(raytraceresult);
-
-                }
-                this.isAirBorne = true;
-            }
-
-            if (entityraytraceresult == null || this.getPierceLevel() <= 0) {
-                break;
-            }
-
-            raytraceresult = null;
+    @Override
+    public boolean hasNoGravity() {
+        if (this.isInWater()) {
+            return false;
+        } else {
+            return true;
         }
     }
 
-    public double particleDistSq(double toX, double toY, double toZ) {
-        double d0 = getPosX() - toX;
-        double d1 = getPosY() - toY;
-        double d2 = getPosZ() - toZ;
-        return d0 * d0 + d1 * d1 + d2 * d2;
-    }
-
-    protected void arrowHit(LivingEntity living) {
-        super.arrowHit(living);
-        if (living != null && (this.func_234616_v_() == null || !living.isEntityEqual(this.func_234616_v_()))) {
-            if (living instanceof PlayerEntity) {
-                this.damageShield((PlayerEntity) living, (float) this.getDamage());
-            }
-        }
-    }
-
-    protected void damageShield(PlayerEntity player, float damage) {
-        if (damage >= 3.0F && player.getActiveItemStack().getItem().isShield(player.getActiveItemStack(), player)) {
-            ItemStack copyBeforeUse = player.getActiveItemStack().copy();
-            int i = 1 + MathHelper.floor(damage);
-            player.getActiveItemStack().damageItem(i, player, (p_213833_1_) -> {
-                p_213833_1_.sendBreakAnimation(Hand.MAIN_HAND);
-            });
-            if (player.getActiveItemStack().isEmpty()) {
-                Hand Hand = player.getActiveHand();
-                net.minecraftforge.event.ForgeEventFactory.onPlayerDestroyItem(player, copyBeforeUse, Hand);
-
-                if (Hand == Hand.MAIN_HAND) {
-                    this.setItemStackToSlot(EquipmentSlotType.MAINHAND, ItemStack.EMPTY);
-                } else {
-                    this.setItemStackToSlot(EquipmentSlotType.OFFHAND, ItemStack.EMPTY);
-                }
-                player.resetActiveHand();
-                this.playSound(SoundEvents.ITEM_SHIELD_BREAK, 0.8F, 0.8F + this.world.rand.nextFloat() * 0.4F);
-            }
-        }
+    @Override
+    public boolean isPushedByWater() {
+        return false;
     }
 
     @OnlyIn(Dist.CLIENT)
@@ -168,122 +126,8 @@ public class MasterSwordBeamEntity extends AbstractArrowEntity {
         return 1.0F;
     }
 
-    public boolean hasNoGravity() {
-        return true;
-    }
-
-    @Override
-    protected ItemStack getArrowStack() {
-        return ItemStack.EMPTY;
-    }
-
     @Override
     public IPacket<?> createSpawnPacket() {
         return NetworkHooks.getEntitySpawningPacket(this);
-    }
-
-    private IntOpenHashSet piercedEntities;
-    private List<Entity> hitEntities;
-    private int knockbackStrength;
-
-    public void setKnockbackStrength(int knockbackStrengthIn) {
-        this.knockbackStrength = knockbackStrengthIn;
-    }
-
-    protected void onImpact (RayTraceResult result) {
-        super.onImpact(result);
-        this.remove();
-    }
-
-    protected void onEntityHit(EntityRayTraceResult p_213868_1_) {
-        Entity entity = p_213868_1_.getEntity();
-        float f = (float)this.getMotion().length();
-        int i = MathHelper.ceil(Math.max((double)f * this.getDamage(), 0.0D));
-        if (this.getPierceLevel() > 0) {
-            if (this.piercedEntities == null) {
-                this.piercedEntities = new IntOpenHashSet(5);
-            }
-
-            if (this.hitEntities == null) {
-                this.hitEntities = Lists.newArrayListWithCapacity(5);
-            }
-
-            if (this.piercedEntities.size() >= this.getPierceLevel() + 1) {
-                this.remove();
-                return;
-            }
-
-            this.piercedEntities.add(entity.getEntityId());
-        }
-
-        if (this.getIsCritical()) {
-            i += this.rand.nextInt(i / 2 + 2);
-        }
-
-        Entity entity1 = this.func_234616_v_();
-        DamageSource damagesource = DamageSource.MAGIC;
-        if (entity1 != null) {
-            if (entity1 instanceof LivingEntity) {
-                if (entity1 instanceof LivingEntity) {
-                    damagesource = DamageSource.causeIndirectMagicDamage(this, entity1);
-                    ((LivingEntity) entity1).setLastAttackedEntity(entity);
-                }
-            }
-        }
-
-        boolean flag = entity.getType() == EntityType.ENDERMAN;
-        int j = entity.getFireTimer();
-        if (this.isBurning() && !flag) {
-            entity.setFire(5);
-        }
-
-        if (entity.attackEntityFrom(damagesource, (float)i)) {
-            if (flag) {
-                return;
-            }
-
-            if (entity instanceof LivingEntity) {
-                LivingEntity livingentity = (LivingEntity)entity;
-                if (!this.world.isRemote && this.getPierceLevel() <= 0) {
-                    //   livingentity.setArrowCountInEntity(livingentity.getArrowCountInEntity() + 1);
-                }
-
-                if (this.knockbackStrength > 0) {
-                    Vector3d vec3d = this.getMotion().mul(1.0D, 0.0D, 1.0D).normalize().scale((double)this.knockbackStrength * 0.6D);
-                    if (vec3d.lengthSquared() > 0.0D) {
-                        livingentity.addVelocity(vec3d.x, 0.1D, vec3d.z);
-                    }
-                }
-
-                this.arrowHit(livingentity);
-                if (entity1 != null && livingentity != entity1 && livingentity instanceof PlayerEntity && entity1 instanceof ServerPlayerEntity) {
-                    ((ServerPlayerEntity)entity1).connection.sendPacket(new SChangeGameStatePacket(SChangeGameStatePacket.field_241770_g_, 0.0F));
-                }
-
-                if (!entity.isAlive() && this.hitEntities != null) {
-                    this.hitEntities.add(livingentity);
-                }
-
-                if (!this.world.isRemote && entity1 instanceof ServerPlayerEntity) {
-                    ServerPlayerEntity serverplayerentity = (ServerPlayerEntity)entity1;
-                }
-            }
-
-            this.playSound(this.getHitGroundSound(), 1.0F, 1.2F / (this.rand.nextFloat() * 0.2F + 0.9F));
-            if (this.getPierceLevel() <= 0) {
-                this.remove();
-            }
-        } else {
-            this.setMotion(this.getMotion().scale(-0.1D));
-            //this.ticksInAir = 0;
-            if (!this.world.isRemote && this.getMotion().lengthSquared() < 1.0E-7D) {
-                if (this.pickupStatus == AbstractArrowEntity.PickupStatus.ALLOWED) {
-                    this.entityDropItem(this.getArrowStack(), 0.1F);
-                }
-
-                this.remove();
-            }
-        }
-
     }
 }
