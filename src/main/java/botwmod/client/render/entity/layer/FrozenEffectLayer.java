1707fd5608032b065b07a56c118cbbef595ded6c
Mozz123 <68307526+Mozz123@users.noreply.github.com>
2021-02-26 15:02:47 -0700

    test commit

--- src/main/java/botwmod/client/render/entity/layer/FrozenEffectLayer.java
+++ src/main/java/botwmod/client/render/entity/layer/FrozenEffectLayer.java
@@ -8,6 +8,7 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.IEntityRenderer;
 import net.minecraft.client.renderer.entity.LivingRenderer;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.client.renderer.entity.model.EntityModel;
@@ -19,7 +20,7 @@ import net.minecraft.util.ResourceLocation;
 import java.util.function.Predicate;
 
 public class FrozenEffectLayer extends LayerRenderer<LivingEntity, EntityModel<LivingEntity>> {
-    private static final ResourceLocation FROZEN_LAYER_TEXTURE = new ResourceLocation(BotwMod.MODID, "textures/entity/frozen_layer.png");
+    private static final RenderType TEXTURE = RenderType.getEntitySmoothCutout(new ResourceLocation("botwmod:textures/entity/frozen_layer.png"));
     private final LivingRenderer<LivingEntity, EntityModel<LivingEntity>> renderer;
 
     public FrozenEffectLayer(LivingRenderer<LivingEntity, EntityModel<LivingEntity>> renderer) {
@@ -30,11 +31,8 @@ public class FrozenEffectLayer extends LayerRenderer<LivingEntity, EntityModel<L
     @Override
     public void render(MatrixStack matrix, IRenderTypeBuffer buffer, int packedLightIn, LivingEntity entity, float limbSwing, float limbSwingAmount, float partialTicks, float age, float netHeadYaw, float headPitch) {
         if (entity.isPotionActive(ModEffects.FROZEN_EFFECT.get())) {
-            EntityModel model = this.getEntityModel();
-            IVertexBuilder ivertexbuilder = buffer.getBuffer(RenderType.getEntityTranslucent(FROZEN_LAYER_TEXTURE));
-
-            model.setRotationAngles(entity, limbSwing, limbSwingAmount, age, netHeadYaw, headPitch);
-            model.render(matrix, ivertexbuilder, packedLightIn, 0, 1, 1, 1, 1);
+            IVertexBuilder ivertexbuilder = buffer.getBuffer(TEXTURE);
+            this.getEntityModel().render(matrix, ivertexbuilder, packedLightIn, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
 
         }
     }
--- src/main/java/botwmod/setup/ClientEventHandler.java
+++ src/main/java/botwmod/setup/ClientEventHandler.java
@@ -10,13 +10,21 @@ import botwmod.registry.ModEntities;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.LivingRenderer;
-import net.minecraft.client.renderer.entity.model.EntityModel;
-import net.minecraft.item.ItemStack;
+import net.minecraft.client.renderer.entity.ZombieRenderer;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.monster.ZombieEntity;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderLivingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.IRenderFactory;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraft.entity.LivingEntity;
+import org.lwjgl.system.CallbackI;
+
+import java.util.Map;
 
 @Mod.EventBusSubscriber(modid = BotwMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientEventHandler {
@@ -26,11 +34,23 @@ public class ClientEventHandler {
         RenderingRegistry.registerEntityRenderingHandler(ModEntities.BOMB.get(), manager -> new BombEntityRender(manager));
         RenderingRegistry.registerEntityRenderingHandler(ModEntities.BOMB_ARROW.get(), manager -> new BombArrowRender(manager));
         RenderingRegistry.registerEntityRenderingHandler(ModEntities.ICE_ARROW.get(), manager -> new IceArrowRender(manager));
-        for (EntityRenderer<?> e : Minecraft.getInstance().getRenderManager().renderers.values()) {
-            if (e instanceof LivingRenderer) {
-                ((LivingRenderer) e).addLayer(new FrozenEffectLayer((LivingRenderer) e));
+        /* for (Map.Entry<EntityType<?>, EntityRenderer<?>> entry : Minecraft.getInstance().getRenderManager().renderers.entrySet()) {
+            EntityRenderer render = entry.getValue();
+            if (render instanceof LivingRenderer) {
+                ((LivingRenderer) render).addLayer(new FrozenEffectLayer((LivingRenderer) render));
             }
+        }
+         */
+    }
 
+    @SubscribeEvent
+    public void renderLivingEvent (RenderLivingEvent event) {
+        LivingRenderer render = event.getRenderer();
+        LivingEntity entity = event.getEntity();
+        if (entity instanceof ZombieEntity) {
+            if (render instanceof ZombieRenderer) {
+                render.addLayer(new FrozenEffectLayer(render));
+            }
         }
     }
 }
