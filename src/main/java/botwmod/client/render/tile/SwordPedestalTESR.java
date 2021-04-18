b084c9f70886a58f79aba794165a20b765d03d19
Mozz123 <68307526+Mozz123@users.noreply.github.com>
2021-04-17 19:23:53 -0600

    Made the Sword Pedestal's animation more like the Shulker Box's

--- src/main/java/botwmod/client/render/tile/SwordPedestalTESR.java
+++ src/main/java/botwmod/client/render/tile/SwordPedestalTESR.java
@@ -10,8 +10,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.entity.EnderDragonRenderer;
-import net.minecraft.client.renderer.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.item.ItemStack;
